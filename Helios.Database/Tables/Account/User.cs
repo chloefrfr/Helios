2c27fe964b4466130590945e0e832648ada4f091
chloe <118663359+chloefrfr@users.noreply.github.com>
2025-03-27 02:04:15 -0400

    actual table

--- Helios.Database/Tables/Account/User.cs
+++ Helios.Database/Tables/Account/User.cs
@@ -9,16 +9,20 @@ namespace Helios.Database.Tables.Account
         [Key] 
         [Column("id")] 
         public int Id { get; set; }
-
-        [Column("username")] 
-        public string Username { get; set; }
-
-        [Column("email")]
+        [Column("email")] 
         [EmailAddress]
         public string Email { get; set; }
-
         [Column("password")]
         [DataType(DataType.Password)] 
         public string Password { get; set; }
+        [Column("accountId")]
+        [Required]
+        public string AccountId { get; set; }
+        [Column("discordId")]
+        [Required]
+        public string DiscordId { get; set; }
+        [Column("banned")]
+        [Required]
+        public bool Banned { get; set; }
     }
 }
