56240b7b42722cd5382ed6f0324e1f06a1d020f3
chloe <118663359+chloefrfr@users.noreply.github.com>
2025-04-06 15:19:31 -0400

    waitingroom

---
+++ Helios/Controllers/WaitingRoomController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Helios.Controllers;
+
+[ApiController]
+public class WaitingRoomController : ControllerBase
+{
+    [HttpGet("/waitingroom/api/waitingroom")]
+    public IActionResult WaitingRoom()
+    {
+        return Ok(new
+        {
+            retryTime = 4,
+            expectedWait = 4
+        });
+    }
+
+    [HttpGet("/launcher-resources/waitingroom/Fortnite/retryconfig.json")]
+    [HttpGet("/launcher-resources/waitingroom/retryconfig.json")]
+    public IActionResult GetRetryConfig()
+    {
+        return Ok(new
+        {
+            maxRetryCount = 1,
+            retryInterval = 10,
+            retryJitter = 69,
+            failAction = "ABORT"
+        });
+    }
+}
