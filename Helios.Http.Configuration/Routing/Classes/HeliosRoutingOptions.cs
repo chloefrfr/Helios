7dbac36f1191b9cfbb80c100c91a45073192744a
chloe <118663359+chloefrfr@users.noreply.github.com>
2025-04-07 16:45:04 -0400

    Delete Helios.Http.Configuration directory

--- Helios.Http.Configuration/Routing/Classes/HeliosRoutingOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Helios.Http.Configuration.Routing.Classes;
-
-public class HeliosRoutingOptions
-{
-    public bool EnableDetailedErrors { get; set; } = false;
-    public bool EnablePerformanceLogging { get; set; } = false;
-}
