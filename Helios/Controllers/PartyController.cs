dafdb98565e981274690e1aae2f877fc3dd15b40
chloe <118663359+chloefrfr@users.noreply.github.com>
2025-05-09 20:31:20 -0400

    Create PartyController.cs

---
+++ Helios/Controllers/PartyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Helios.Controllers;
+
+[ApiController]
+[Route("/party/api/v1/")]
+public class PartyController : ControllerBase
+{
+    [HttpGet("Fortnite/user/{accountId}")]
+    public async Task<IActionResult> GetUser(string accountId)
+    {
+        return NoContent();
+    }
+}
